5d5482c2439892d3593341ad235b082d4d186b50
BilliStrom <165456158+BilliStrom@users.noreply.github.com>
2025-03-02 07:38:45 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// Конфигурация Firebase
+const firebaseConfig = {
+    apiKey: "ВАШ_API_KEY",
+    authDomain: "ВАШ_AUTH_DOMAIN",
+    projectId: "ВАШ_PROJECT_ID",
+    storageBucket: "ВАШ_STORAGE_BUCKET",
+    messagingSenderId: "ВАШ_MESSAGING_SENDER_ID",
+    appId: "ВАШ_APP_ID"
+};
+
+// Инициализация Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
